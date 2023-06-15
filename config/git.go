ed2992ff201361ed67a3663c16b7ca61de337736
isksss <104404522+isksss@users.noreply.github.com>
2023-06-15 12:15:22 +0900

    git commands

--- config/git.go
+++ config/git.go
@@ -10,3 +10,42 @@ func IsGitInstalled() bool {
 	}
 	return true
 }
+
+type GitConfig struct {
+	Key   string
+	Value string
+}
+
+func SetGitConfig(config GitConfig) error {
+	cmd := exec.Command("git", "config", config.Key, config.Value)
+	return cmd.Run()
+}
+
+func GetGitConfig(config GitConfig) (string, error) {
+	cmd := exec.Command("git", "config", config.Key)
+	out, err := cmd.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(out), nil
+}
+
+func SetGitUser(name string) error {
+	config := GitConfig{
+		Key:   "user.name",
+		Value: name,
+	}
+
+	cmd := exec.Command("git", "config", "--global", config.Key, config.Value)
+	return cmd.Run()
+}
+
+func SetGitEmail(email string) error {
+	config := GitConfig{
+		Key:   "user.email",
+		Value: email,
+	}
+
+	cmd := exec.Command("git", "config", "--global", config.Key, config.Value)
+	return cmd.Run()
+}
